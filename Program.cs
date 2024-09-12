4c3f27909ae72e4b5977f36261dba1403b33e42d
Khaled.Nabil <Khaled.Nabil@KHALED_NABIL>
2024-09-12 17:01:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using IDP.Data;
+using IdentityServer4.Models;
+using Microsoft.Extensions.DependencyInjection;
+using IDP;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+builder.Services.AddIdentityServer()
+    .AddInMemoryClients(config.Clients)
+    .AddInMemoryIdentityResources(config.IdentityResources)
+    .AddInMemoryApiResources(config.ApiResources)
+    .AddInMemoryApiScopes(config.ApiScopes)
+    .AddTestUsers(config.Users)
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseIdentityServer();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
